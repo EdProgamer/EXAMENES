5ff63f937c84b3f99181c205932714f317970c65
EdProgamer <edyncanap@gmail.com>
2026-07-14 14:10:03 -0500

    algunos estilos añadidos

--- script.js
+++ script.js
@@ -16,4 +16,17 @@ document.getElementById('formDNI').addEventListener('submit', function(e) {
     document.getElementById('resultado').innerText = 
       'DNI no válido';
   }
-});
+
+  if (resultado) {
+  const div = document.getElementById('resultado');
+  div.innerText = `Tu puntaje obtenido es: ${resultado.Puntaje}`;
+  div.style.backgroundColor = "#d4edda"; // verde suave
+  div.style.borderColor = "#28a745";     // verde intenso
+} else {
+  const div = document.getElementById('resultado');
+  div.innerText = "DNI no válido";
+  div.style.backgroundColor = "#f8d7da"; // rojo suave
+  div.style.borderColor = "#dc3545";     // rojo intenso
+}
+
+  });
